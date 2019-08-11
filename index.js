f3aabebf062253b7b1272a5f0507e05289c88b47
Maксим Вовк <mvx.vovk@gmail.com>
2019-08-11 11:32:39 +0300

    Fixed listener

--- index.js
+++ index.js
@@ -1,36 +1,101 @@
 const express = require('express')
+const port = 3000
 const app = express()
-const port = 4000
 
-app.use(express.static(__dirname + '/static'))
-app.set('view engine', 'ejs')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:admin@test-cqqsr.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin2:admin@clustertest-mse2m.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+
 client.connect(err => {
-    console.log('MongoDB error: ' + err)
-    const collection = client.db("step_project").collection("notes");
+    console.log('BD connect error: ', err)
+    const collection = client.db("step").collection("notes");
     app.db = collection
+        
+//   client.close();
 });
 
-app.get('/', async (request, response) => {
-    let note = []
-    await app.db.find({}).forEach(el => {
-        note.push(el)
-    })
-    response.render('index', {note})
-})
+app.use(express.static(__dirname + "/static"))
 
-app.get('/:id', async (request, response) => {
-    let note = []
-    await app.db.find({id: request.params.id}).forEach((el) => {
-        note.push(el)
+app.set("view engine", "ejs")
+
+
+app.get("/", async (req, res)=>{
+    let notes = []
+    await app.db.find({}).forEach((el) => {
+        notes.push(el)
     });
-    response.render('note', {note})
+
+    res.render("index", {notes})
+
 })
 
-app.listen(port, () => {
-    console.log('Server: ON')
+app.post("/delete", async (req, res) => {
+    // Выводим данные запроса
+    console.log(req.body.id)
+    try {
+        // Удаляем заметку по id
+        await app.db.deleteOne({
+            id: req.body.id
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({deleted: true})
 })
+
+
+app.post("/create", async (req, res) => {
+    // Выводим данные из body в post
+    console.log(req.body)
+    try {
+        // Создаем в базе заметку
+        await app.db.insertOne({
+            ...req.body 
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({created: true})
+})
+
+app.post("/edit", async (req, res) => {
+    // Выводим данные из body в post
+    console.log(req.body)
+    try {
+        // Создаем в базе заметку
+        await app.db.updateOne({
+            id: req.body.id,
+
+        },
+        {
+            $set: {
+                title: req.body.title,
+                text: req.body.text
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({edited: true})
+})
+
+app.get('/:id', async (req, res) => {
+    let note;
+    await app.db.find({id: req.params.id }).forEach((el) => {
+        note = el
+    });
+
+    res.render("note", {note})
+})
+
+app.listen(port, ()=>{
+    console.log("hello in console")
+})
+
